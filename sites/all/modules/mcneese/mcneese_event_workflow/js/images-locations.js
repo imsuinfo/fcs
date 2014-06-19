128b6304e532a821503671b57142b7acfbda8c6f
Kevin Day <kday@mcneese.edu>
2014-06-19 12:05:26 -0500

    Feature: add support for adding images on the side of the location selection, currently using example images

---
+++ sites/all/modules/mcneese/mcneese_event_workflow/js/images-locations.js
@@ -0,0 +1,44 @@
+(function ($) {
+  Drupal.behaviors.mcneese_event_workflow_images_location = {
+    attach:function(context) {
+      $('#field-event-location-0 > .form-item.form-type-radio > input', context).each(function() {
+        var item = $(this);
+
+        $(item).click(function() {
+          $('#field-event-location-0 > .location-image', context).each(function() {
+            if ($(this).hasClass('location-image-visible')) {
+              $(this).removeClass('location-image-visible');
+            }
+
+            if (!$(this).hasClass('location-image-invisible')) {
+              $(this).addClass('location-image-invisible');
+            }
+          });
+
+          if ($(item).val() == 'SEED') {
+            $('#field-event-location-0-image-1', context).each(function() {
+              if ($(this).hasClass('location-image-invisible')) {
+                $(this).removeClass('location-image-invisible');
+              }
+
+              if (!$(this).hasClass('location-image-visible')) {
+                $(this).addClass('location-image-visible');
+              }
+            });
+          }
+          else {
+            $('#field-event-location-0-image-0', context).each(function() {
+              if ($(this).hasClass('location-image-invisible')) {
+                $(this).removeClass('location-image-invisible');
+              }
+
+              if (!$(this).hasClass('location-image-visible')) {
+                $(this).addClass('location-image-visible');
+              }
+            });
+          }
+        });
+      });
+    }
+  }
+})(jQuery);
