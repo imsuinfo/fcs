9297e692acf091f3f61a591d3e65ae721247fa8b
Kevin Day <thekevinday@gmail.com>
2015-03-25 13:45:47 -0500

    Update Core: drupal 7.35

--- misc/states.js
+++ misc/states.js
@@ -131,17 +131,9 @@ states.Dependent.prototype = {
    */
   compare: function (reference, selector, state) {
     var value = this.values[selector][state.name];
-    var name = reference.constructor.name;
-
-    // some browsers, like IE8+, need additional help to work as expected.
-    if (!name) {
-      name = $.type(reference);
-      name = name.charAt(0).toUpperCase() + name.slice(1);
-    }
-
-    if (name in states.Dependent.comparisons) {
+    if (reference.constructor.name in states.Dependent.comparisons) {
       // Use a custom compare function for certain reference value types.
-      return states.Dependent.comparisons[name](reference, value);
+      return states.Dependent.comparisons[reference.constructor.name](reference, value);
     }
     else {
       // Do a plain comparison otherwise.
