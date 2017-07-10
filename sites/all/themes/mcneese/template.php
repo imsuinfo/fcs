30dfc59cbb229bbaee6f9fd0a9c3fc8fa9c977c5
Kevin Day <kday@mcneese.edu>
2017-07-10 11:18:17 -0500

    Cleanup: move current default theme into a subdirectory called 'classic'
    Signed-off-by: Kevin Day <kday@mcneese.edu>

--- sites/all/themes/mcneese/template.php
+++ sites/all/themes/mcneese/template.php
@@ -1624,7 +1624,7 @@ function mcneese_cf_theme_get_variables_alter(&$cf, $vars) {
 
     $custom_css = array();
     $custom_css['options'] = array('type' => 'file', 'group' => CSS_THEME, 'every_page' => TRUE, 'weight' => 5, 'media' => 'all', 'preprocess' => FALSE);
-    $custom_css['data'] = $cf['theme']['path'] . '/css/workaround/ie.css';
+    $custom_css['data'] = $cf['theme']['path'] . '/css/classic/workaround/ie.css';
     drupal_add_css($custom_css['data'], $custom_css['options']);
 
     if ($cf['agent']['major_version'] != 'unknown' && $cf['agent']['major_version'] <= 8) {
@@ -1640,7 +1640,7 @@ function mcneese_cf_theme_get_variables_alter(&$cf, $vars) {
       else {
         $custom_css = array();
         $custom_css['options'] = array('type' => 'file', 'group' => CSS_THEME, 'every_page' => TRUE, 'weight' => 5, 'media' => 'all', 'preprocess' => FALSE);
-        $custom_css['data'] = $cf['theme']['path'] . '/css/workaround/ie-legacy.css';
+        $custom_css['data'] = $cf['theme']['path'] . '/css/classic/workaround/ie-legacy.css';
         drupal_add_css($custom_css['data'], $custom_css['options']);
       }
 
